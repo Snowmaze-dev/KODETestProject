b7c7d55987055a301a19497786d50c5ee3b753bb
Snowmazes <snowmaze@mail.ru>
2020-11-08 18:04:33 +0200

    Bottom nav and toolbar

--- app/src/main/java/ru/snowmaze/kodetestproject/ui/MainActivity.kt
+++ app/src/main/java/ru/snowmaze/kodetestproject/ui/MainActivity.kt
@@ -1,20 +1,45 @@
 package ru.snowmaze.kodetestproject.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import by.kirich1409.viewbindingdelegate.viewBinding
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.fxn.BubbleTabBar
+import com.fxn.OnBubbleClickListener
 import ru.snowmaze.kodetestproject.R
-import ru.snowmaze.kodetestproject.databinding.ActivityMainBinding
+import ru.snowmaze.kodetestproject.ui.classes.ClassesFragment
 import ru.snowmaze.kodetestproject.ui.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
-    private val binding: ActivityMainBinding by viewBinding()
+    private val homeFragment = HomeFragment()
+    private val classesFragment = ClassesFragment()
+    private var previousFragment: Fragment = homeFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction().add(R.id.fragments_container, HomeFragment()).commit()
+        supportFragmentManager.beginTransaction().add(R.id.fragments_container, homeFragment)
+            .add(R.id.fragments_container, classesFragment).hide(classesFragment).commit()
+        findViewById<BubbleTabBar>(R.id.bottom_navigation).addBubbleListener(object :
+            OnBubbleClickListener {
+            override fun onBubbleClick(id: Int) {
+                if (id == R.id.home_fragment) {
+                    openFragment(homeFragment)
+                }
+                if (id == R.id.classes_fragment) {
+                    openFragment(classesFragment)
+                }
+            }
+
+        })
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .setCustomAnimations(R.anim.fade_in, R.anim.fade_out)
+            .hide(previousFragment).show(fragment)
+            .commit()
+        previousFragment = fragment
     }
 
 }
---
+++ app/src/main/java/ru/snowmaze/kodetestproject/ui/classes/ClassesFragment.kt
@@ -0,0 +1,6 @@
+package ru.snowmaze.kodetestproject.ui.classes
+
+import androidx.fragment.app.Fragment
+
+class ClassesFragment: Fragment() {
+}
